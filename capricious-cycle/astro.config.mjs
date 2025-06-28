32a876f9c7e7e40a849ed58e0f85be535bd8ddc8
solomon0256 <wenyuhe03@gmail.com>
2025-06-29 01:32:53 +0900

    feat: 在 astro.config.mjs 中添加空行以提高可读性

--- capricious-cycle/astro.config.mjs
+++ capricious-cycle/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+
 export default defineConfig({
     envPrefix: ['PUBLIC_', 'ASTRO_'],
     integrations: [tailwind()],
