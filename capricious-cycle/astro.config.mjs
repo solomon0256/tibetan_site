9dd000e64e1b826be4b8c0d304fb659abf3f3ad1
solomon0256 <wenyuhe03@gmail.com>
2025-06-30 22:32:32 +0900

    Remove @astrojs/cloudflare from devDependencies in package.json

--- capricious-cycle/astro.config.mjs
+++ capricious-cycle/astro.config.mjs
@@ -1,13 +1,11 @@
 // Instrument Sentry before everything else
 import './instrument.mjs';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    integrations: [
-      tailwind(),
-      cloudflare(),
-    ],
-  });
- 
+  output: 'static',
+  integrations: [
+    tailwind(),
+  ],
+});
