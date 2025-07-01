2c80e400a9fffd780332f78c5456c672af150dd9
solomon0256 <wenyuhe03@gmail.com>
2025-07-01 14:23:07 +0900

    Add Sentry dependencies and comment out initialization in hooks.server.js
    - Added @sentry/browser and @sentry/vite-plugin to dependencies and devDependencies in package.json.
    - Commented out Sentry initialization code in hooks.server.js to prevent automatic setup if ASTRO_SENTRY_DSN is not provided.

--- capricious-cycle/src/hooks.server.js
+++ capricious-cycle/src/hooks.server.js
@@ -1,17 +1,17 @@
 // src/hooks.server.js
 import * as Sentry from '@sentry/node';
 
-if (process.env.ASTRO_SENTRY_DSN) {
-  console.log('⎘ ASTRO_SENTRY_DSN=', process.env.ASTRO_SENTRY_DSN);
-  console.log('⎘ ASTRO_NODE_ENV=', process.env.ASTRO_NODE_ENV);
-  Sentry.init({
-    dsn: process.env.ASTRO_SENTRY_DSN,
-    environment: process.env.ASTRO_NODE_ENV,
-    tracesSampleRate: 0.1,
-  });
-} else {
-  console.warn('⚠️ 未检测到 ASTRO_SENTRY_DSN，Sentry 未初始化');
-}
+// if (process.env.ASTRO_SENTRY_DSN) {
+//   console.log('⎘ ASTRO_SENTRY_DSN=', process.env.ASTRO_SENTRY_DSN);
+//   console.log('⎘ ASTRO_NODE_ENV=', process.env.ASTRO_NODE_ENV);
+//   Sentry.init({
+//     dsn: process.env.ASTRO_SENTRY_DSN,
+//     environment: process.env.ASTRO_NODE_ENV,
+//     tracesSampleRate: 0.1,
+//   });
+// } else {
+//   console.warn('⚠️ 未检测到 ASTRO_SENTRY_DSN，Sentry 未初始化');
+// }
 
 export function onError({ error }) {
   Sentry.captureException(error);
