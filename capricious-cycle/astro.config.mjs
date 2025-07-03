c150b19addfd013ad884040b51b3502a2fed4047
solomon0256 <wenyuhe03@gmail.com>
2025-07-03 15:24:17 +0900

    chore: add @astrojs/cloudflare dependency to package.json

--- capricious-cycle/astro.config.mjs
+++ capricious-cycle/astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+  adapter: cloudflare(),
   integrations: [tailwind()]
 });
